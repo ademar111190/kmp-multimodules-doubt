4329d9499ce924111802dd48cf8a38a611730012
Ademar Alves de Oliveira <ademar111190@gmail.com>
2020-05-01 16:51:58 -0300

    Fix / workaround

--- ios/app/Hello World/ContentView.swift
+++ ios/app/Hello World/ContentView.swift
@@ -1,7 +1,10 @@
 import SwiftUI
+import FatModule
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        let helloWorld = HelloWorld(message: "Greetings from iOS!")
+        let text = "\(helloWorld.message)\n\(helloWorld.egassem())"
+        return Text(text)
     }
 }
